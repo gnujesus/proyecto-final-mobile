f72b55900a8b6180c15fa5d46ced4dc55bfed51e
neutralAMG <amoscoso698@gmail.com>
2025-04-20 22:44:11 -0400

    feat: fix to the shelter view so theres no more warning about unique keys.

--- app/views/shelters.tsx
+++ app/views/shelters.tsx
@@ -33,11 +33,11 @@ export default function Albergues() {
         Lista de Albergues
       </Text>
       <ScrollView showsVerticalScrollIndicator={false}>
-        {shelters.map((item) => (
-          <View className="bg-white rounded-xl shadow-md p-4 mb-4">
+        {shelters.map((item, index) => (
+          <View key={index} className="bg-white rounded-xl shadow-md p-4 mb-4">
              <Text className="text-xl font-bold  text-center mb-6">
-        {item.codigo}
-      </Text>
+              {item.codigo}
+               </Text>
             <Text className="text-gray-700 mb-1">Coordinador {item.coordinador}</Text>
             <Text className="text-gray-700 mb-1">📍 {item.edificio}</Text>
             <Text className="text-gray-700 mb-1">📞 {item.telefono}</Text>
